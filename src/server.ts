95b1613638241d4fed9e4c5e98188d891c048e83
Dusan Dodic <duledodicc@gmail.com>
2021-11-24 05:15:18 +0100

    Allowed CORS

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import { randomPersona } from './endpoints/randomPersona.js';
 import { randomName } from './endpoints/randomName.js';
 import { randomOccupation } from './endpoints/randomOccupation.js';
@@ -14,6 +15,7 @@ const dataPath = process.env.DATA_PATH || './data';
 const avatarURL = process.env.AVATAR_URL || 'https://avatars.dicebear.com/api';
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 
 // Routes
